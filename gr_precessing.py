36008e7e8db97bf14c4e46f75545446001934e42
scottperkins <scottperkins2@montana.edu>
2019-01-07 12:57:06 -0700

    work on precessing

--- gr_precessing.py
+++ gr_precessing.py
@@ -1,4 +1,5 @@
 import utilities as util
+import numpy as np
 import gr 
 import astropy.cosmology as cosmology
 from astropy.coordinates import Distance
@@ -18,7 +19,12 @@ class IMRPhenomPv3(gr.IMRPhenomD):
     def __init__(self, mass1, mass2,spin1,spin2, collision_time, \
                     collision_phase,Luminosity_Distance,cosmo_model = cosmology.Planck15,
                     NSflag = False,N_detectors=1):
-        super(IMRPhenomPv3,self).__init__( mass1=mass1, mass2=mass2,spin1=spin1,spin2=spin2,
+
+        spin1_mag = np.sqrt(np.sum(np.asarray([x**2 for x in spin1])))
+        spin2_mag = np.sqrt(np.sum(np.asarray([x**2 for x in spin2])))
+        spin1_vec = spin1
+        spin2_vec = spin2
+        super(IMRPhenomPv3,self).__init__( mass1=mass1, mass2=mass2,spin1=spin1_mag,spin2=spin2_mag,
                                              collision_time=collision_time, 
                                                 collision_phase=collision_phase,
                                                 Luminosity_Distance=Luminosity_Distance, 
@@ -32,7 +38,7 @@ class IMRPhenomPv3(gr.IMRPhenomD):
 if __name__=='__main__':
     m1 = 10*s_solm
     m2 = 5*s_solm
-    spin1 = .9
-    spin2 = 0.4
+    spin1 = np.asarray([.3,.4,.5])
+    spin2 = np.asarray([.6,.1,.1])
     dl = 100*mpc
     model = IMRPhenomPv3(mass1=m1,mass2=m2, spin1=spin1,spin2=spin2,collision_time=0,collision_phase=0, Luminosity_Distance=dl, NSflag = False, N_detectors=1)
